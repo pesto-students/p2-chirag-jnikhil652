86022b6dbe4409e03f42794ebcb1c4f0ca58a093
Nikhil Jadhav <jnikhil652@gmail.com>
2022-05-30 23:56:04 +0530

    assinments

---
+++ Week-3/Assignment 3.3/index.js
@@ -0,0 +1,22 @@
+function createStack() {
+    // let items = [];
+    let items = [];
+    return {
+        get items() {
+            return items;
+        },
+        push(item) {
+            this.items.push(item);
+        },
+        pop() {
+            return this.items.pop();
+        },
+    };
+}
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+console.log(stack.pop()); // => 5
+console.log(stack.items); // => [10]
+stack.items = [10, 100, 1000]; // Encapsulation broke
+console.log(stack.items);
