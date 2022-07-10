8372b3213aa39f5881f77af1db65cd6172284ef7
Nikhil Jadhav <jnikhil652@gmail.com>
2022-07-10 17:09:44 +0530

    Assignment 9

---
+++ Week-9/todo/src/todo.js
@@ -0,0 +1,15 @@
+
+
+export default function Todo ({todoList,completeTodo}){
+    return(
+        <table>
+            {todoList.map(element=>{
+                return (
+                <tr className="todoitem" key={element.id}>
+                <td><input type="checkbox" onClick={completeTodo} data-id={element.id} checked={(element.completed) ? "checked" : ""} disabled={(element.completed) ? "disabled" : ""}/></td>
+                <td className={(element.completed) ? "completed" : ""}>{element.name}</td>
+            </tr>);
+            })}
+        </table>
+    );
+}
